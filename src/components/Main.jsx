a72bc2bfffea497271a0f04588c570e14ba8a025
Mauricio Lopes <mlopeesz@gmail.com>
2022-05-18 11:51:24 -0300

    feat: component Main with Navbar

---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Navbar from './subcomponents/Navbar';
+
+export default function Main() {
+  return (
+    <div id="Main" className="h-screen">
+      <Navbar />
+    </div>
+  );
+}
