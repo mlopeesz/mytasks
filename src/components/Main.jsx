31682a74d8595e4e8f772e6edb79d7f4e5c020de
Mauricio Lopes <mlopeesz@gmail.com>
2022-05-18 19:52:26 -0300

    feat: add Tasks in Main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './subcomponents/Navbar';
 import AddTask from './subcomponents/AddTask';
+import Tasks from './subcomponents/Tasks';
 
 export default function Main() {
   return (
     <div id="Main" className="h-screen">
       <Navbar />
       <AddTask />
+      <Tasks />
     </div>
   );
 }
