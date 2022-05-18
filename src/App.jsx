5cc973e4941d19d05a662f87823b28a34ede80e7
Mauricio Lopes <mlopeesz@gmail.com>
2022-05-18 10:50:04 -0300

    feat: import Home in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>MyTasks</h1>
+    <div className="App bg-light-palette">
+      <Home />
     </div>
   );
 }
