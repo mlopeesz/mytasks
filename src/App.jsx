dc2c60701d1474ededf26a94af4506e2e6c5254b
Mauricio Lopes <mlopeesz@gmail.com>
2022-05-18 11:51:51 -0300

    fix: link and font

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Home from './components/Home';
+import Main from './components/Main';
 
 function App() {
   return (
     <div className="App bg-light-palette">
       <Home />
+      <Main />
     </div>
   );
 }
