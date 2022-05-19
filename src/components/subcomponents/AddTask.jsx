002ae4c3683d93640e0e0acd94a1a20467a793e1
Mauricio Lopes <mlopeesz@gmail.com>
2022-05-19 10:51:07 -0300

    feat: input

--- src/components/subcomponents/AddTask.jsx
+++ src/components/subcomponents/AddTask.jsx
@@ -10,15 +10,26 @@ export default function AddTask() {
   }
 
   function handleClick() {
-    setTasks([...tasks, { id: Math.random(), name: taskInput }]);
-    setTaskInput('');
+    if (taskInput) {
+      setTasks([...tasks, { id: Math.random(), name: taskInput }]);
+      setTaskInput('');
+    }
+    alert('Cannot add empty task!');
   }
 
   return (
     <div className="flex justify-center m-10">
+      <input
+        className="mx-2 px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-purple-palette focus:outline-none"
+        onChange={handleChange}
+        value={taskInput}
+        type="text"
+        placeholder="Clean dog poop"
+      />
       <button
         type="button"
         className="bg-purple-palette hover:bg-purple-hover-palette rounded-full px-3 mx-2 text-white text-[24px] transition duration-300"
+        onClick={handleClick}
       >
         +
       </button>
@@ -29,7 +40,6 @@ export default function AddTask() {
       >
         Add New Task
       </button>
-      <input onChange={handleChange} value={taskInput} type="text" />
     </div>
   );
 }
