07028b462ac817ace22a9758159832f6bea386c1
Mauricio Lopes <mlopeesz@gmail.com>
2022-05-19 10:51:16 -0300

    feat: css

--- src/components/subcomponents/Tasks.jsx
+++ src/components/subcomponents/Tasks.jsx
@@ -7,12 +7,14 @@ export default function Tasks() {
 
   return (
     tasks.map((task) => (
-      <div key={task.id} className="flex justify-center gap-4">
-        <div className="flex gap-2 items-center">
-          <img className="w-10" src={trashIcon} alt="Trash Icon" />
-          <input className="" type="checkbox" />
+      <div key={task.id} className="flex justify-start gap-4 my-4 max-w-[65%] mx-auto">
+        <div className="flex gap-4 items-center">
+          <img className="w-8 transition hover:scale-110" src={trashIcon} alt="Trash Icon" />
+          <input className="form-check-input h-4 w-4 border rounded-sm focus:outline-none transition duration-300 align-center cursor-pointer" type="checkbox" />
         </div>
-        <p className="text-[24px]">{task.name}</p>
+        <ul>
+          <li className="text-[24px] place-self-start">{task.name}</li>
+        </ul>
       </div>
     ))
   );
